73554894e54511c0f5e79d0ca6d15ddaa01b5e64
3dzzh <1275354807@qq.com>
2018-08-28 10:50:22 +0800

    first-commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 class Product extends Component {
-    addToCart = id => {
-        // console.log(id)
-        const { addToCart } = this.props
-        addToCart(id)
-        // 如何在组建内定义一个全局的属性 使用 this.xxx 定义
-        console.log(this.inp)
-        // console.log(document.getElementById('btn'))
-      }
+     addToCart = id => {
+    // console.log(id)
+    const { addToCart } = this.props
+    addToCart(id)
+    // 如何在组建内定义一个全局的属性 使用 this.xxx 定义
+    console.log(this.inp)
+    // console.log(document.getElementById('btn'))
+  }
     render() {
-        const {product } = this.props
-        const ProductsList = product.map(goods =>(<li key={goods.id}>
+        const {Product } = this.state
+        const ProductsList = Product.map(goods =>(<li key={goods.id}>
         <p style={{marginBottom:0}}>{goods.goodsName} - ${goods.price.toFixed(2)} x {goods.inventory}</p>
         <button disabled={!goods.inventory} onClick={this.AddToCart}>add to cart</button>
        </li>))
@@ -22,5 +22,4 @@ class Product extends Component {
         );
     }
 }
-
 export default Product;
