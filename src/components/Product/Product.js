ee2aa1719032cf1f345411b624f1d4b39b2dae94
3dzzh <1275354807@qq.com>
2018-08-28 10:18:27 +0800

    updata-commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,16 @@
 import React, { Component } from 'react';
 class Product extends Component {
-    state={
-        Product:[
-            {
-                id:10,
-                goodsName:'ipad',
-                price:300,
-                inventory:3
-            },
-            {
-                id:11,
-                goodsName:'ipone',
-                price:500,
-                inventory:2
-            }
-        ]
-    }
-    AddToCart  = id=>{
-        const {Product} = this.state
-        this.setState({
-            Product: Product.map(goods =>{
-                if(goods.id === id){
-                    goods.inventory = goods.inventory > 0 ? goods.inventory-1 : 0
-                }
-                return goods
-            })
-        })
-        const  AddToCart= this.props
-        AddToCart(id) 
-    }
+    addToCart = id => {
+        // console.log(id)
+        const { addToCart } = this.props
+        addToCart(id)
+        // 如何在组建内定义一个全局的属性 使用 this.xxx 定义
+        console.log(this.inp)
+        // console.log(document.getElementById('btn'))
+      }
     render() {
-        const {Product } = this.state
-        const ProductsList = Product.map(goods =>(<li key={goods.id}>
+        const {product } = this.props
+        const ProductsList = product.map(goods =>(<li key={goods.id}>
         <p style={{marginBottom:0}}>{goods.goodsName} - ${goods.price.toFixed(2)} x {goods.inventory}</p>
         <button disabled={!goods.inventory} onClick={this.AddToCart}>add to cart</button>
        </li>))
